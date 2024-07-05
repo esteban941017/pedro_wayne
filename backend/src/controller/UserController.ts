76dfe6eb9b7f269950195d263b62e548a0907c08
Esteban Ramirez <esteban941017@gmail.com>
2024-07-05 16:15:35 -0300

    adjusted user create return

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -9,12 +9,13 @@ const userRepository = new UserRepository(connection);
 
 routes.post('/create', async (req, res) => {
   try {
+    console.log(req.body);
     if (!req.body.username || !req.body.password || !req.body.type) return res.status(422).json('Missing parameters');
     if (req.body.type !== 'funcionario' && req.body.type !== 'gerente' && req.body.type !== 'admin segurança') return res.status(422).json('Invalid user type');
 
     const inputCreateUser = User.create(req.body.username, req.body.password, req.body.type);
     const outputCreateUser = await userRepository.create(inputCreateUser);
-    return res.status(201).json(outputCreateUser);
+    return res.status(201).json({ userId: outputCreateUser });
   } catch (error) {
     console.error(error);
     return res.status(500).json('Internal server error');
